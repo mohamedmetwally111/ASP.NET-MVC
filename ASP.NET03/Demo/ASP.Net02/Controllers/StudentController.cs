60a53099dd8d3cb57a5f6fff7bc2f3e41670c81d
mohamedmetwally111 <metwallym527@gmail.com>
2026-03-26 17:44:26 +0200

    Upload full folder ASP.NET03

---
+++ ASP.NET03/Demo/ASP.Net02/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using ASP.Net02.Models;
+using ASP.NET02.Models;
+using ASP.NET02.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Net02.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentBL studentbl = new StudentBL();
+        DepartmentBL departmentbl = new DepartmentBL();
+
+        //   /student/ShowAll  
+        public IActionResult ShowAll()
+        {
+
+            List<Student> students=studentbl.GetAll();
+            return View("ShowAll", students);
+
+        }
+
+        //    /Student/ShowDetails?id=3 
+        public IActionResult ShowDetails(int id)
+        {
+            Student students = studentbl.GetById(id);
+            return View("ShowDetails", students);
+        }
+
+    }
+}
---
+++ ASP.NET03/Demo/ASP.Net02/Models/StudentBL.cs
@@ -0,0 +1,20 @@
+﻿using ASP.Net02.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.Net02.Models
+{
+    public class StudentBL
+    {
+        CompanyDbContext context=new CompanyDbContext();
+
+        public List<Student> GetAll()
+        {
+            return context.Students.Include(s=>s.Departments).ToList();
+        }
+
+        public Student GetById(int id)
+        {
+            return context.Students.Include(s => s.Departments).FirstOrDefault(s => s.Id == id);
+        }
+    }
+}
